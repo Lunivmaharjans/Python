8cc74fae947d5ee0958d7eff5881f9ba93282d5b
Luniv Maharjan <lunivmaharjan1@gmail.com>
2026-04-24 08:47:57 +0545

    first comment

---
+++ day5.py
@@ -0,0 +1,13 @@
+# import mathu as mu
+
+# print(mu.add(1,2))
+# import os 
+# import sys
+# from mathu import * # all linxa
+# print(add(1,2))
+# print(sub(3,2))
+
+
+def day():
+    print("hello from day 5")
+day()
---
+++ test.py
@@ -0,0 +1,18 @@
+import day5 as da
+import mathu as mu
+
+# print(da)
+# print("your calculation of add is ",mu.add(1,2))
+
+
+def execute():
+    da
+    mu.add(1,2)
+    mu.mult(2,2)
+
+if __name__ == "__main__":
+    execute()
+
+
+# task
+# circular import
