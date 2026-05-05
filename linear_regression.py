97f72b9c2c87ec9df5a435d572f29398b59690a1
Luniv Maharjan <lunivmaharjan1@gmail.com>
2026-05-05 11:24:17 +0545

    day15

--- linear_regression.py
+++ linear_regression.py
@@ -2,50 +2,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def gradient_descent1(w_now, b_now, points1, learning_rate):
-    w_gradient1 = 0
-    b_gradient1 = 0
 
-    n = len(points1)
-    
-    for i in range(n):
-        x = points1.iloc[i].Temperature 
-        y = points1.iloc[i].Revenue 
-
-        w_gradient1 += (-2/n) * x *(y-(w_now * x + b_now))
-        b_gradient1 += (-2/n) * (y-(w_now *x + b_now))
-    w1 = w_now - learning_rate * w_gradient1
-    b1 = b_now -learning_rate * b_gradient1
-
-    return w1,b1
-    
-class LinearRegrassionModel:
+class linearregressionmodel:
     def _init_(self, learning_rate = 0.001, epochs = 1000, history_interval = 100):
         # self.weight = 0
         # self.bias = 0
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.history_interval = history_interval
-
-
-    def mse(self, points1, w1, b1,x_col = None ,y_col = None):
-        total_error = 0
+        
+    def gradient_descent(self, w_now, b_now, points, x_col = None, y_col = None):
         if x_col is None and y_col is None:
-            columns =points1.columns.to_list()
+            columns = points.columns.to_list()
             x_col = columns[0]
             y_col = columns[1]
-        for i in range(len(points1)):
-            x = points1.iloc[i].x
-            y = points1.iloc[i].y
-            total_error += (y - (w1*x + b1)) **2
-        return total_error / len(points1)
+        
+        w_gradient = 0
+        b_gradient = 0
+    
+        n = len(points)
+    
+        for i in range(n):
+            x = points.iloc[i][x_col]
+            y = points.iloc[i][y_col]
+            residual = (y - (w_now * x + b_now))
+        
+        w_gradient += (-2/n) * x * residual
+        b_gradient += (-2/n) * residual
+        
+        w = w_now - self.learning_rate * w_gradient
+        b = b_now - self.learning_rate * b_gradient
+        
+    def mse(self, points, w, b, x_col = None, y_col = None):
+        if x_col is None and y_col is None:
+            columns = points.columns.to_list()
+            x_col = columns[0]
+            y_col = columns[1]
+        
+        total_error = 0
+        for i in range(len(points)):
+            x = points.iloc[i][x_col]
+            y = points.iloc[i][y_col]
+        total_error += (y - (w*x + b)) **2
+        return total_error / len(points)
     
     
 
-model = LinearRegrassionModel()
-points1 = pd.read_csv("IceCreamData.csv")
+model = linearregressionmodel()
+points = pd.read_csv("IceCreamData.csv")
+        
+w = 0
+b = 0
 
-w1 = 0
-b1 = 0
-error = model.msc(points1,w1,b1,x_col="Temperature", y_col = "Revenue")
+error = model.mse(points, w, b)
+# error = model.mse(points, w, b, x_col = "Temperature", y_col = "Revenue")
+
 print(error)
